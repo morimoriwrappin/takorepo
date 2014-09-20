8be602b655a916bce9863cb0dee5cf0605827f7d
kentana20 <civic.b16b@gmail.com>
2014-09-20 12:45:07 +0900

    railsひながた作った

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Takorepo::Application.config.secret_key_base = '57e337c1af8ca87e9115ecdbf73251655174cc13c41434c015ebd8622d7c672ae1e3786719f0cf576eb300a9d502d6590942e9041b6d40e0902aaa97897af692'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Takorepo::Application.config.session_store :cookie_store, key: '_takorepo_session'
